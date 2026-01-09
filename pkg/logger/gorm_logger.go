63a957cfca325d0a129f7a2cdf4537b1b73a636c
Harald Fielker <harald.fielker@gmail.com>
2026-01-09 17:30:14 +0100

    logger package

--- pkg/logger/gorm_logger.go
+++ pkg/logger/gorm_logger.go
@@ -1,4 +1,4 @@
-package database
+package logger
 
 import (
 	"context"
@@ -8,17 +8,17 @@ import (
 	"time"
 
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+	gormlogger "gorm.io/gorm/logger"
 )
 
 type slogGormLogger struct {
 	logger                    *slog.Logger
-	LogLevel                  logger.LogLevel
+	LogLevel                  gormlogger.LogLevel
 	IgnoreRecordNotFoundError bool
 	SlowThreshold             time.Duration
 }
 
-func newSlogGormLogger(l *slog.Logger, lvl logger.LogLevel) logger.Interface {
+func NewSlogGormLogger(l *slog.Logger, lvl gormlogger.LogLevel) gormlogger.Interface {
 	return &slogGormLogger{
 		logger:                    l.With("component", "database"),
 		LogLevel:                  lvl,
@@ -27,32 +27,32 @@ func newSlogGormLogger(l *slog.Logger, lvl logger.LogLevel) logger.Interface {
 	}
 }
 
-func (l *slogGormLogger) LogMode(level logger.LogLevel) logger.Interface {
+func (l *slogGormLogger) LogMode(level gormlogger.LogLevel) gormlogger.Interface {
 	newLogger := *l
 	newLogger.LogLevel = level
 	return &newLogger
 }
 
 func (l *slogGormLogger) Info(ctx context.Context, msg string, data ...interface{}) {
-	if l.LogLevel >= logger.Info {
+	if l.LogLevel >= gormlogger.Info {
 		l.logger.InfoContext(ctx, fmt.Sprintf(msg, data...))
 	}
 }
 
 func (l *slogGormLogger) Warn(ctx context.Context, msg string, data ...interface{}) {
-	if l.LogLevel >= logger.Warn {
+	if l.LogLevel >= gormlogger.Warn {
 		l.logger.WarnContext(ctx, fmt.Sprintf(msg, data...))
 	}
 }
 
 func (l *slogGormLogger) Error(ctx context.Context, msg string, data ...interface{}) {
-	if l.LogLevel >= logger.Error {
+	if l.LogLevel >= gormlogger.Error {
 		l.logger.ErrorContext(ctx, fmt.Sprintf(msg, data...))
 	}
 }
 
 func (l *slogGormLogger) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error) {
-	if l.LogLevel <= logger.Silent {
+	if l.LogLevel <= gormlogger.Silent {
 		return
 	}
 
@@ -60,18 +60,18 @@ func (l *slogGormLogger) Trace(ctx context.Context, begin time.Time, fc func() (
 	sql, rows := fc()
 
 	if err != nil && (!l.IgnoreRecordNotFoundError || !errors.Is(err, gorm.ErrRecordNotFound)) {
-		if l.LogLevel >= logger.Error {
+		if l.LogLevel >= gormlogger.Error {
 			l.logger.ErrorContext(ctx, "query error", "err", err, "elapsed", elapsed, "rows", rows, "sql", sql)
 		}
 		return
 	}
 
-	if l.SlowThreshold != 0 && elapsed > l.SlowThreshold && l.LogLevel >= logger.Warn {
+	if l.SlowThreshold != 0 && elapsed > l.SlowThreshold && l.LogLevel >= gormlogger.Warn {
 		l.logger.WarnContext(ctx, "slow query", "elapsed", elapsed, "rows", rows, "sql", sql)
 		return
 	}
 
-	if l.LogLevel >= logger.Info {
+	if l.LogLevel >= gormlogger.Info {
 		l.logger.InfoContext(ctx, "query", "elapsed", elapsed, "rows", rows, "sql", sql)
 	}
 }
