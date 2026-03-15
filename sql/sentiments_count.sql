b0c98e1172220ea4b5aec972bcfd15d64145e07b
Harald Fielker <harald.fielker@gmail.com>
2026-03-15 10:40:16 +0100

    added sentiments_deframed

--- sql/sentiments_count.sql
+++ sql/sentiments_count.sql
@@ -1,4 +1,7 @@
 SELECT
     COUNT(*) FILTER (WHERE sentiments = '{}'::jsonb) AS empty_sentiments_trend_count,
-    COUNT(*) FILTER (WHERE sentiments <> '{}'::jsonb) AS non_empty_sentiments_trend_count
+    COUNT(*) FILTER (WHERE sentiments <> '{}'::jsonb) AS non_empty_sentiments_trend_count,
+
+    COUNT(*) FILTER (WHERE sentiments_deframed = '{}'::jsonb) AS empty_sentiments_deframed_trend_count,
+    COUNT(*) FILTER (WHERE sentiments_deframed <> '{}'::jsonb) AS non_empty_sentiments_deframed_trend_count
 FROM trends;
