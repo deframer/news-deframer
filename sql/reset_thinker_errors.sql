f4acf33b4bf625e352f5565197037b4f292e31c1
Harald Fielker <harald.fielker@gmail.com>
2026-04-06 17:33:57 +0200

    updated statement

--- sql/reset_thinker_errors.sql
+++ sql/reset_thinker_errors.sql
@@ -1,7 +1,8 @@
 BEGIN TRANSACTION;
 
 -- SELECT count(id) FROM items WHERE think_error_count > 3;
+-- SELECT count(id) FROM items WHERE think_error_count = 0 and think_error IS NOT NULL;
 
-UPDATE items SET think_error_count = 0 WHERE think_error_count > 3;
+UPDATE items SET think_error_count = 0, think_error = NULL WHERE think_error_count > 3;
 
 ROLLBACK;
