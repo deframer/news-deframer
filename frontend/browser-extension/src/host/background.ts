e7a3687ad03edddfb5fb6b3bb566a32ebb6c46bd
Harald Fielker <harald.fielker@gmail.com>
2026-04-30 12:14:40 +0200

    Revert "connection indicator"
    This reverts commit 14ff11d3e7026781c5778d8a9cfadec4455c7d2f.

--- frontend/browser-extension/src/host/background.ts
+++ frontend/browser-extension/src/host/background.ts
@@ -1,44 +1,8 @@
 import log from '../shared/logger';
-import { CONNECTION_TIMEOUT_MS, getSettings } from '../shared/settings';
+import { CONNECTION_TIMEOUT_MS } from '../shared/settings';
 import { ProxyResponse } from '../shared/types';
 
 log.info('Background script running');
-
-const DEFAULT_ICON_PATH = {
-  16: 'assets/browser-extension/icon16.png',
-  32: 'assets/browser-extension/icon32.png',
-  48: 'assets/browser-extension/icon48.png',
-  128: 'assets/browser-extension/icon128.png',
-} as const;
-
-const ENABLED_ICON_PATH = {
-  16: 'assets/browser-extension/icon-enabled16.png',
-  32: 'assets/browser-extension/icon-enabled32.png',
-  48: 'assets/browser-extension/icon-enabled48.png',
-  128: 'assets/browser-extension/icon-enabled128.png',
-} as const;
-
-const syncActionIcon = async () => {
-  const settings = await getSettings();
-  await chrome.action.setIcon({ path: settings.enabled ? ENABLED_ICON_PATH : DEFAULT_ICON_PATH });
-};
-
-void syncActionIcon();
-
-chrome.runtime.onInstalled.addListener(() => {
-  void syncActionIcon();
-});
-
-chrome.runtime.onStartup.addListener(() => {
-  void syncActionIcon();
-});
-
-chrome.storage.onChanged.addListener((changes, areaName) => {
-  if (areaName !== 'local') return;
-  if (changes.enabled) {
-    void syncActionIcon();
-  }
-});
 
 chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
   if (request.type === 'SET_BYPASS') {
