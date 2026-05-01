4261aad454698b414287c148c4e82097117bc1cf
Harald Fielker <harald.fielker@gmail.com>
2026-05-01 09:15:46 +0200

    media fix

--- pkg/syncer/media.go
+++ pkg/syncer/media.go
@@ -1,9 +1,14 @@
 package syncer
 
 import (
-	"net/url"
+	"fmt"
 	"strconv"
 	"strings"
+
+	"github.com/deframer/news-deframer/pkg/database"
+	"github.com/mmcdole/gofeed"
+	ext "github.com/mmcdole/gofeed/extensions"
+	"net/url"
 
 	"golang.org/x/net/html"
 )
@@ -54,6 +59,225 @@ func transformContent(content string) (MediaData, error) {
 	f(doc)
 
 	return data, nil
+}
+
+func extractFromContentTag(item *gofeed.Item) *MediaData {
+	if item == nil || item.Content == "" {
+		return nil
+	}
+	mediaData, err := transformContent(item.Content)
+	if err == nil && mediaData.URL != "" {
+		return &mediaData
+	}
+	return nil
+}
+
+func extractFromEnclosureTag(item *gofeed.Item) *MediaData {
+	if item == nil {
+		return nil
+	}
+	for _, enc := range item.Enclosures {
+		if strings.HasPrefix(enc.Type, "image/") && enc.URL != "" {
+			w, h := parseDimensions(enc.URL)
+			return &MediaData{
+				URL:    enc.URL,
+				Medium: "image",
+				Width:  w,
+				Height: h,
+			}
+		}
+	}
+	return nil
+}
+
+func extractFromDescriptionFallback(res *database.ThinkResult) *MediaData {
+	if res == nil {
+		return nil
+	}
+
+	target := res.DescriptionOriginal
+	mediaData, err := transformContent(target)
+	if err != nil || mediaData.URL == "" {
+		target = res.TitleOriginal
+		mediaData, err = transformContent(target)
+	}
+
+	if err == nil && mediaData.URL != "" {
+		return &mediaData
+	}
+	return nil
+}
+
+func applyMediaData(item *gofeed.Item, data *MediaData) {
+	if item == nil || data == nil {
+		return
+	}
+	if item.Extensions == nil {
+		item.Extensions = make(map[string]map[string][]ext.Extension)
+	}
+	if item.Extensions["media"] == nil {
+		item.Extensions["media"] = make(map[string][]ext.Extension)
+	}
+
+	// <media:credit> always delete this
+	delete(item.Extensions["media"], "credit")
+
+	attrs := map[string]string{
+		"url":    data.URL,
+		"medium": data.Medium,
+	}
+	if data.Width > 0 {
+		attrs["width"] = fmt.Sprintf("%d", data.Width)
+	}
+	if data.Height > 0 {
+		attrs["height"] = fmt.Sprintf("%d", data.Height)
+	}
+
+	item.Extensions["media"]["content"] = []ext.Extension{{
+		Name:  "content",
+		Attrs: attrs,
+	}}
+
+	if len(data.Alt) > 0 {
+		item.Extensions["media"]["description"] = []ext.Extension{{
+			Name:  "description",
+			Value: data.Alt,
+		}}
+	}
+}
+
+func extractMediaContent(item *gofeed.Item) (*database.MediaContent, error) {
+	if item == nil || item.Extensions == nil {
+		return nil, nil
+	}
+
+	mediaExt, ok := item.Extensions["media"]
+	if !ok {
+		return nil, nil
+	}
+
+	contentExt := findBestMediaExtension(mediaExt, "content")
+	if contentExt == nil {
+		contentExt = findBestMediaExtension(mediaExt, "thumbnail")
+	}
+	if contentExt == nil {
+		return nil, nil
+	}
+
+	mc := &database.MediaContent{
+		URL:    contentExt.Attrs["url"],
+		Type:   contentExt.Attrs["type"],
+		Medium: contentExt.Attrs["medium"],
+	}
+
+	if mc.Medium == "" {
+		if strings.HasPrefix(mc.Type, "image/") {
+			mc.Medium = "image"
+		} else if strings.HasPrefix(mc.Type, "video/") {
+			mc.Medium = "video"
+		}
+	}
+	if mc.Medium == "" && contentExt.Name == "thumbnail" {
+		mc.Medium = "image"
+	}
+
+	w, _ := strconv.Atoi(contentExt.Attrs["width"])
+	h, _ := strconv.Atoi(contentExt.Attrs["height"])
+	if w == 0 || h == 0 {
+		w, h = parseDimensions(mc.URL)
+	}
+	mc.Width = w
+	mc.Height = h
+
+	if titles, ok := contentExt.Children["title"]; ok && len(titles) > 0 {
+		mc.Title = titles[0].Value
+	}
+	if descs, ok := contentExt.Children["description"]; ok && len(descs) > 0 {
+		mc.Description = descs[0].Value
+	}
+	if credits, ok := contentExt.Children["credit"]; ok && len(credits) > 0 {
+		mc.Credit = credits[0].Value
+	}
+
+	if mc.Title == "" {
+		if titles, ok := mediaExt["title"]; ok && len(titles) > 0 {
+			mc.Title = titles[0].Value
+		}
+	}
+	if mc.Description == "" {
+		if descs, ok := mediaExt["description"]; ok && len(descs) > 0 {
+			mc.Description = descs[0].Value
+		}
+	}
+	if mc.Credit == "" {
+		if credits, ok := mediaExt["credit"]; ok && len(credits) > 0 {
+			mc.Credit = credits[0].Value
+		}
+	}
+
+	if thumbs, ok := mediaExt["thumbnail"]; ok && len(thumbs) > 0 {
+		tExt := thumbs[0]
+		if tURL := tExt.Attrs["url"]; tURL != "" {
+			mt := &database.MediaThumbnail{URL: tURL}
+			tw, _ := strconv.Atoi(tExt.Attrs["width"])
+			th, _ := strconv.Atoi(tExt.Attrs["height"])
+			if tw == 0 || th == 0 {
+				tw, th = parseDimensions(tURL)
+			}
+			mt.Width = tw
+			mt.Height = th
+			mc.Thumbnail = mt
+		}
+	}
+
+	return mc, nil
+}
+
+func findBestMediaExtension(exts map[string][]ext.Extension, name string) *ext.Extension {
+	var best *ext.Extension
+	for _, values := range exts {
+		best = betterMediaExtension(best, findBestMediaExtensionInList(values, name))
+	}
+
+	return best
+}
+
+func findBestMediaExtensionInList(exts []ext.Extension, name string) *ext.Extension {
+	var best *ext.Extension
+	for i := range exts {
+		if exts[i].Name == name && exts[i].Attrs["url"] != "" {
+			val := exts[i]
+			best = betterMediaExtension(best, &val)
+		}
+		for _, children := range exts[i].Children {
+			best = betterMediaExtension(best, findBestMediaExtensionInList(children, name))
+		}
+	}
+
+	return best
+}
+
+func betterMediaExtension(current, candidate *ext.Extension) *ext.Extension {
+	if candidate == nil {
+		return current
+	}
+	if current == nil {
+		return candidate
+	}
+
+	if mediaExtensionScore(candidate) > mediaExtensionScore(current) {
+		return candidate
+	}
+	return current
+}
+
+func mediaExtensionScore(ext *ext.Extension) int {
+	w, _ := strconv.Atoi(ext.Attrs["width"])
+	h, _ := strconv.Atoi(ext.Attrs["height"])
+	if w == 0 || h == 0 {
+		w, h = parseDimensions(ext.Attrs["url"])
+	}
+	return w * h
 }
 
 // parseDimensions pulls width from query string and calculates 16:9 height
--- pkg/syncer/media_test.go
+++ pkg/syncer/media_test.go
@@ -3,6 +3,9 @@ package syncer
 import (
 	"testing"
 
+	"github.com/deframer/news-deframer/pkg/database"
+	"github.com/mmcdole/gofeed"
+	ext "github.com/mmcdole/gofeed/extensions"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -62,6 +65,336 @@ func TestTransformContent(t *testing.T) {
 			got, err := transformContent(tt.content)
 			assert.NoError(t, err)
 			assert.Equal(t, tt.expected, got)
+		})
+	}
+}
+
+func TestExtractMediaContent(t *testing.T) {
+	tests := []struct {
+		name     string
+		item     *gofeed.Item
+		expected *database.MediaContent
+	}{
+		{
+			name:     "No Extensions",
+			item:     &gofeed.Item{},
+			expected: nil,
+		},
+		{
+			name: "Full Media Content",
+			item: &gofeed.Item{
+				Extensions: map[string]map[string][]ext.Extension{
+					"media": {
+						"content": {{
+							Name: "content",
+							Attrs: map[string]string{
+								"url":    "http://example.com/video.mp4",
+								"type":   "video/mp4",
+								"medium": "video",
+								"width":  "1920",
+								"height": "1080",
+							},
+							Children: map[string][]ext.Extension{
+								"title":  {{Name: "title", Value: "Video Title"}},
+								"credit": {{Name: "credit", Value: "Getty Images"}},
+							},
+						}},
+						"thumbnail": {{
+							Name: "thumbnail",
+							Attrs: map[string]string{
+								"url":    "http://example.com/thumb.jpg",
+								"width":  "300",
+								"height": "200",
+							},
+						}},
+					},
+				},
+			},
+			expected: &database.MediaContent{
+				URL:    "http://example.com/video.mp4",
+				Type:   "video/mp4",
+				Medium: "video",
+				Width:  1920,
+				Height: 1080,
+				Title:  "Video Title",
+				Credit: "Getty Images",
+				Thumbnail: &database.MediaThumbnail{
+					URL:    "http://example.com/thumb.jpg",
+					Width:  300,
+					Height: 200,
+				},
+			},
+		},
+		{
+			name: "Item Level Credit",
+			item: &gofeed.Item{
+				Extensions: map[string]map[string][]ext.Extension{
+					"media": {
+						"content": {{
+							Name: "content",
+							Attrs: map[string]string{
+								"url":    "http://example.com/image.jpg",
+								"type":   "image/jpeg",
+								"medium": "image",
+							},
+						}},
+						"credit": {{Name: "credit", Value: "AP Photo"}},
+					},
+				},
+			},
+			expected: &database.MediaContent{
+				URL:    "http://example.com/image.jpg",
+				Type:   "image/jpeg",
+				Medium: "image",
+				Width:  1920,
+				Height: 1080,
+				Credit: "AP Photo",
+			},
+		},
+		{
+			name: "Image No Thumbnail",
+			item: &gofeed.Item{
+				Extensions: map[string]map[string][]ext.Extension{
+					"media": {
+						"content": {{
+							Name: "content",
+							Attrs: map[string]string{
+								"url":    "http://example.com/image.jpg",
+								"type":   "image/jpeg",
+								"medium": "image",
+								"width":  "1920",
+								"height": "1080",
+							},
+						}},
+					},
+				},
+			},
+			expected: &database.MediaContent{
+				URL:    "http://example.com/image.jpg",
+				Type:   "image/jpeg",
+				Medium: "image",
+				Width:  1920,
+				Height: 1080,
+			},
+		},
+		{
+			name: "Missing Dimensions (Defaults)",
+			item: &gofeed.Item{
+				Extensions: map[string]map[string][]ext.Extension{
+					"media": {
+						"content": {{
+							Name: "content",
+							Attrs: map[string]string{
+								"url":    "http://example.com/image.jpg",
+								"type":   "image/jpeg",
+								"medium": "image",
+							},
+						}},
+					},
+				},
+			},
+			expected: &database.MediaContent{
+				URL:    "http://example.com/image.jpg",
+				Type:   "image/jpeg",
+				Medium: "image",
+				Width:  1920,
+				Height: 1080,
+			},
+		},
+		{
+			name: "Partial Dimensions (Recalculate from URL)",
+			item: &gofeed.Item{
+				Extensions: map[string]map[string][]ext.Extension{
+					"media": {
+						"content": {{
+							Name: "content",
+							Attrs: map[string]string{
+								"url":    "http://example.com/image.jpg?width=800",
+								"type":   "image/jpeg",
+								"medium": "image",
+								"width":  "800",
+								// height missing
+							},
+						}},
+					},
+				},
+			},
+			expected: &database.MediaContent{
+				URL:    "http://example.com/image.jpg?width=800",
+				Type:   "image/jpeg",
+				Medium: "image",
+				Width:  800,
+				Height: 450, // 800 * 9 / 16
+			},
+		},
+		{
+			name: "Specific Media Content Tag",
+			item: &gofeed.Item{
+				Extensions: map[string]map[string][]ext.Extension{
+					"media": {
+						"content": {{
+							Name: "content",
+							Attrs: map[string]string{
+								"url":        "https://bar/foo.jpg",
+								"type":       "image/jpeg",
+								"expression": "full",
+								"width":      "931",
+								"height":     "523",
+							},
+						}},
+					},
+				},
+			},
+			expected: &database.MediaContent{
+				URL:    "https://bar/foo.jpg",
+				Type:   "image/jpeg",
+				Medium: "image",
+				Width:  931,
+				Height: 523,
+			},
+		},
+		{
+			name: "Media Group Content",
+			item: &gofeed.Item{
+				Extensions: map[string]map[string][]ext.Extension{
+					"media": {
+						"group": {{
+							Name: "group",
+							Children: map[string][]ext.Extension{
+								"content": {{
+									Name: "content",
+									Attrs: map[string]string{
+										"url":        "https://foo.jpg",
+										"type":       "image/jpeg",
+										"medium":     "image",
+										"expression": "full",
+										"width":      "300",
+										"height":     "180",
+									},
+								}},
+							},
+						}},
+					},
+				},
+			},
+			expected: &database.MediaContent{
+				URL:    "https://foo.jpg",
+				Type:   "image/jpeg",
+				Medium: "image",
+				Width:  300,
+				Height: 180,
+			},
+		},
+		{
+			name: "Media Group Prefers Larger Rendition",
+			item: &gofeed.Item{
+				Extensions: map[string]map[string][]ext.Extension{
+					"media": {
+						"group": {{
+							Name: "group",
+							Children: map[string][]ext.Extension{
+								"content": {
+									{
+										Name: "content",
+										Attrs: map[string]string{
+											"url":        "https://example.com/image-large.jpg",
+											"type":       "image/jpeg",
+											"medium":     "image",
+											"expression": "full",
+											"width":      "870",
+											"height":     "522",
+										},
+									},
+									{
+										Name: "content",
+										Attrs: map[string]string{
+											"url":        "https://example.com/image-small.jpg",
+											"type":       "image/jpeg",
+											"medium":     "image",
+											"expression": "full",
+											"width":      "300",
+											"height":     "180",
+										},
+									},
+								},
+							},
+						}},
+					},
+				},
+			},
+			expected: &database.MediaContent{
+				URL:    "https://example.com/image-large.jpg",
+				Type:   "image/jpeg",
+				Medium: "image",
+				Width:  870,
+				Height: 522,
+			},
+		},
+		{
+			name: "Fallback to Thumbnail",
+			item: &gofeed.Item{
+				Extensions: map[string]map[string][]ext.Extension{
+					"media": {
+						"thumbnail": {{
+							Name: "thumbnail",
+							Attrs: map[string]string{
+								"url":    "https://ichef.bbci.co.uk/ace/standard/240/cpsprodpb/039e/live/9ebb4470-f496-11f0-a422-4ba8a094a8fa.jpg",
+								"width":  "240",
+								"height": "135",
+							},
+						}},
+					},
+				},
+			},
+			expected: &database.MediaContent{
+				URL:    "https://ichef.bbci.co.uk/ace/standard/240/cpsprodpb/039e/live/9ebb4470-f496-11f0-a422-4ba8a094a8fa.jpg",
+				Medium: "image", // Default injected
+				Width:  240,
+				Height: 135,
+				Thumbnail: &database.MediaThumbnail{
+					URL:    "https://ichef.bbci.co.uk/ace/standard/240/cpsprodpb/039e/live/9ebb4470-f496-11f0-a422-4ba8a094a8fa.jpg",
+					Width:  240,
+					Height: 135,
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := extractMediaContent(tt.item)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.expected, got)
+		})
+	}
+}
+
+func TestParseDimensions(t *testing.T) {
+	tests := []struct {
+		name   string
+		url    string
+		width  int
+		height int
+	}{
+		{
+			name:   "Width Query Param",
+			url:    "https://example.com/image.jpg?width=800",
+			width:  800,
+			height: 450,
+		},
+		{
+			name:   "Invalid URL Defaults",
+			url:    "%",
+			width:  1920,
+			height: 1080,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w, h := parseDimensions(tt.url)
+			assert.Equal(t, tt.width, w)
+			assert.Equal(t, tt.height, h)
 		})
 	}
 }
