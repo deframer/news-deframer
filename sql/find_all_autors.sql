6087c345f7fa97e2ae74a0a3160ba95b5e454f40
Harald Fielker <harald.fielker@gmail.com>
2026-06-03 11:29:32 +0200

    authors

--- sql/find_all_autors.sql
+++ sql/find_all_autors.sql
@@ -1,24 +1,35 @@
-WITH expanded_authors AS (
+WITH config AS (
   SELECT
-    feeds.root_domain AS domain,
+    365 AS days,
+    5 AS min_articles,
+    'de' AS language,
+    'de' AS country
+),
+expanded_authors AS (
+  SELECT
+    feeds.root_domain AS root_domain,
     items.think_rating,
+    items.pub_date,
     UNNEST(items.authors) AS author
   FROM items
   JOIN feeds ON feeds.id = items.feed_id
-  WHERE feeds.language = 'en'
-    -- AND feeds.country = 'de'
+  CROSS JOIN config
+  WHERE feeds.language = config.language
+    AND feeds.country = config.country
     AND feeds.enabled = TRUE
     AND feeds.deleted_at IS NULL
+    AND items.pub_date >= NOW() - (config.days * INTERVAL '1 DAY')
 )
 SELECT
-  domain,
+  root_domain,
   author,
+  MIN(pub_date) AS first_publication_date,
   COUNT(*) AS article_count
 FROM expanded_authors
-WHERE domain IS NOT NULL
-  AND domain <> ''
+WHERE root_domain IS NOT NULL
+  AND root_domain <> ''
   AND author IS NOT NULL
   AND author <> ''
-GROUP BY domain, author
-HAVING COUNT(*) > 5
-ORDER BY domain ASC, author ASC;
+GROUP BY root_domain, author
+HAVING COUNT(*) >= (SELECT min_articles FROM config)
+ORDER BY root_domain ASC, author ASC;
