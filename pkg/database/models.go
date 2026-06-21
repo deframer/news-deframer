9955ccd345d36c56825b609d505309af784599c7
Harald Fielker <harald.fielker@gmail.com>
2026-06-21 10:48:33 +0200

    format fix

--- pkg/database/models.go
+++ pkg/database/models.go
@@ -54,9 +54,9 @@ type FeedError struct {
 }
 
 type StopWords struct {
-	ID        uuid.UUID      `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
-	CreatedAt time.Time      `gorm:"not null;default:now()"`
-	UpdatedAt time.Time      `gorm:"not null;default:now()"`
+	ID        uuid.UUID   `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
+	CreatedAt time.Time   `gorm:"not null;default:now()"`
+	UpdatedAt time.Time   `gorm:"not null;default:now()"`
 	Language  string      `gorm:"type:char(2);not null;uniqueIndex:idx_stop_words_language_global,where:feed_id IS NULL;uniqueIndex:idx_stop_words_language_feed,where:feed_id IS NOT NULL" json:"language"`
 	FeedID    *uuid.UUID  `gorm:"type:uuid;index;uniqueIndex:idx_stop_words_language_feed,where:feed_id IS NOT NULL" json:"feed_id,omitempty"`
 	NounStems StringArray `gorm:"type:text[];not null;default:'{}'" json:"noun_stems"`
